069e3f3f4dd5f7715cd3793626935116093610e8
Wolfyxon <wolfyxon@gmail.com>
2026-01-01 15:05:04 +0100

    Respecting the settings

--- extension/global/main.js
+++ extension/global/main.js
@@ -1,48 +1,59 @@
-let mutationObs;
+async function main() {
+    const settings = await browser.storage.sync.get();
 
-let footerLocationRemoved = false;
-let directionsRemoved = false;
+    let mutationObs;
+    let footerLocationRemoved = false;
+    let directionsRemoved = false;
 
-function run() {
-    const mapsBanner = document.querySelector("*[data-ly]");
+    function run() {
+        const mapsBanner = document.querySelector("*[data-ly]");
 
-    // Your approximate city label on the footer
-    const locationFooterLabel = document.querySelector(".unknown_loc ~ *");
+        // Your approximate city label on the footer
+        const locationFooterLabel = document.querySelector(".unknown_loc ~ *");
 
-    // When searching for towns in your country a widget with directions may appear
-    const directionsWidget = document.querySelector("*[data-attrid='TravelGettingThereFeedback']");
+        // When searching for towns in your country a widget with directions may appear
+        const directionsWidget = document.querySelector("*[data-attrid='TravelGettingThereFeedback']");
 
-    if(locationFooterLabel) {
-        locationFooterLabel.innerText = " ";
-        footerLocationRemoved = true;
-    }
-    
-    if(directionsWidget) {
-        directionsWidget.parentElement.innerHTML = `
-            <div class="googleantidox-hidden-directions">
-                <div>
-                    <b>Directions hidden to protect your privacy.</b>
+        if(locationFooterLabel) {
+            if(!settings.level || settings.level == "city") {
+                locationFooterLabel.innerText = " ";
+            } else {
+                // this can probably be done better
+                locationFooterLabel.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+            }
+
+            footerLocationRemoved = true;
+        }
+        
+        if(directionsWidget) {
+            directionsWidget.parentElement.innerHTML = `
+                <div class="googleantidox-hidden-directions">
+                    <div>
+                        <b>Directions hidden to protect your privacy.</b>
+                    </div>
+                    <div style="font-size: 80%">
+                        GoogleAntiDox
+                    </div>
                 </div>
-                <div style="font-size: 80%">
-                    GoogleAntiDox
-                </div>
-            </div>
-        `;
+            `;
 
-        directionsRemoved = true;
+            directionsRemoved = true;
+        }
+
+        if(footerLocationRemoved && (!mapsBanner || directionsRemoved)) {
+            mutationObs.disconnect();
+        }
     }
 
-    if(footerLocationRemoved && (!mapsBanner || directionsRemoved)) {
-        mutationObs.disconnect();
-    }
+    mutationObs = new MutationObserver(run);
+
+    mutationObs.observe(document, {
+        attributes: true, 
+        childList: false,
+        subtree: true
+    });
+
+    window.addEventListener("load", run);
 }
 
-mutationObs = new MutationObserver(run);
-
-mutationObs.observe(document, {
-    attributes: true, 
-    childList: false,
-  	subtree: true
-});
-
-window.addEventListener("load", run);
+main();
